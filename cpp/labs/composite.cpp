3ea255ed19f7acb6b2215a65a732ac994640f207
Mohamed Saied <infint_hero@hotmail.com>
2022-06-18 12:23:57 +0200

    Session 7

---
+++ cpp/labs/composite.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class PageObject {
+   public:
+      virtual void addItem(PageObject a) { }
+      virtual void removeItem() { }
+      virtual void deleteItem(PageObject a) { }
+};
+
+class Page : public PageObject {
+   public:
+      void addItem(PageObject a) {
+      cout << "Item added into the page" << endl;
+   }
+   void removeItem() {
+      cout << "Item Removed from page" << endl;
+   }
+   void deleteItem(PageObject a) {
+      cout << "Item Deleted from Page" << endl;
+   }
+};
+class Copy : public PageObject {
+   vector<PageObject> copyPages;
+   public:
+      void AddElement(PageObject a) {
+         copyPages.push_back(a);
+      }
+      void addItem(PageObject a) {
+         cout << "Item added to the copy" << endl;
+      }
+      void removeItem() {
+         cout << "Item removed from the copy" << endl;
+      }
+      void deleteItem(PageObject a) {
+         cout << "Item deleted from the copy";
+      }
+};
+int main() {
+   Page p1;
+   Page p2;
+   Copy myCopy;
+   myCopy.AddElement(p1);
+   myCopy.AddElement(p2);
+   myCopy.addItem(p1);
+   p1.addItem(p2);
+   myCopy.removeItem();
+   p2.removeItem();
+}
---
+++ cpp/labs/stack/templates.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "stack.h"
+using namespace std ;
+int main()
+{
+
+	Stack<float> fs(5) ;
+	float f = 1.1 ;
+	cout << "Pushing elements onto fs" << endl ;
+
+	fs.push(5.5);
+	fs.pop(f);
+	cout << f << endl;
+
+	while (fs.push(f))
+	{
+		cout << f << ' ' ;
+		f += 1.1 ;
+	}
+
+	cout << endl << "Stack Full." << endl
+	<< endl << "Popping elements from fs" << endl ;
+	while (fs.pop(f))
+		cout << f << ' ' ;
+	cout << endl << "Stack Empty" << endl ;
+	cout << endl ;
+
+	Stack<int> is ;
+	int i = 1.1 ;
+	cout << "Pushing elements onto is" << endl ;
+	while (is.push(i))
+	{
+		cout << i << ' ' ;
+		i += 1 ;
+	}
+	cout << endl << "Stack Full" << endl
+	<< endl << "Popping elements from is" << endl ;
+	while (is.pop(i))
+			cout << i << ' ' ;
+	cout << endl << "Stack Empty" << endl ;
+
+    return 0;
+}
