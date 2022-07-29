d036594521f3ee119b1d51e218060e52e38db5b5
Mohamed Saied <infint_hero@hotmail.com>
2022-07-29 09:07:11 +0200

    TDD

--- cpp/labs/casting.cpp
+++ cpp/labs/casting.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
+    int num = 20;
+    float bigFloat = num; //No Error : implicit casting
+   
    double x = 1.2;
     // Explicit conversion from double to int
-    int sum = static_cast<int>(x+1);
+    int sum = (int)x; //C-Style
+
+    sum = static_cast<int>(x+1);
 
     std::cout << "Sum = " << sum;
 
